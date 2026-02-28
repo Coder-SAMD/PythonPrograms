b4e09328a52d7220c71d356389755c5fbb52419c
Coder-SAMD <amod.sahasrabudhe.23@gmail.com>
2026-02-28 17:43:27 +0530

    ﻿Merge branch 'main' of https://github.com/Coder-SAMD/Python-Programming # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit. wrote a temperature converter program, with functions and menu.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 #Learning to code with version control systems!
 print("Hello Git and GitHub!!")
+#Program to convert temperature from degree Fahrenheit to degree Celsius
+def f2c():
+    fahrenheit1,fahrenheit2 = eval(input("Enter 2 temperatures in degree Fahrenheit, separated by a comma: "))
+    celsius1 , celsius2 = (fahrenheit1-32)*(5/9) , (fahrenheit2-32)*(5/9)
+    print("The temperatures in degree celsius are: ",celsius1 , " ", celsius2)
+def c2f():
+    celsius1, celsius2 = eval(input("Enter 2 temperatures in degree celsius, separated by a comma: "))
+    fahrenheit1, fahrenheit2 = (1.8*celsius1) + 32 , (1.8*celsius2) + 32
+    print("The temperatures in degree fahrenheit are: ", fahrenheit1, " ", fahrenheit2)
+
+print("This is a temperature converter.")
+user_input = input("Press 1 for converting temperature from degree celsius to degree fahrenheit." +
+                "\nPress 2 for converting temperature from degree fahrenheit to degree celsius.")
+
+if user_input == "1":
+    c2f()
+elif user_input == "2":
+    f2c()
+else:
+    print("Invalid input.Try again; Type the number 1 or 2.")
